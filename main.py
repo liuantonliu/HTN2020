36c7c816cb933c8b6feb7e9a93ce5f66650ce639
Mason <mxmcao@gmail.com>
2021-01-16 23:23:36 -0500

    change method to POST only

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ import json
 
 url = "https://api.nexmo.com/v0.1/messages"
 
+# Mason
+# headers = {
+#   'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2MTA4NDUwMTksImV4cCI6MTYxMTM2MzQxOSwianRpIjoiVDRLS2NCWEpvNHJtIiwiYXBwbGljYXRpb25faWQiOiIyZjA3NmY2Ni05ZmYwLTRhMjMtOWRhNi01Nzg2YTBhYjQwYzYifQ.UjKdfg8G9MZFh8hrtcvURidL0lxrTA4KS1QiE8Rjn6ovpgBpmONKgjOeTVrtMsQmvSd1zsk7Td_onT_FOxX3DyX_FIZ_tCMcyUVKSZIPxYjCeeu_qardEn3mNFF_ZdcQ414cnsos4_EImI41-Ru7NbpXAIQk2YyMER_hU-qYmZRINAt4AXEHo-GA3-rQdIJkfGlLuvwH90sG0ZSWY-k7HWperd3mRiI7wIi88D7g17pGJq-lDLRdFwUbs01fJWDXXV8t8rQyeqNB4pZnw_-dRr8g0Ch-Lu8r1ILYu5BVm7shszFDI_egoG78Qf8NBkicqFXm6a_O31kZbwPJV64LxQ',
+#   'Content-Type': 'application/json'
+# }
 
+# Shyam
 headers = {
   'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2MTA4NDI2MDgsImV4cCI6MTYxMDkwMDIwOCwianRpIjoiaktIMHViZURDY0NJIiwiYXBwbGljYXRpb25faWQiOiI5NWE5ODM3Mi0yNjhmLTRkMTEtYTBlYS0xN2ZhNWZmZjkxYTIifQ.grOU0KnW5lGxomGD5oYWclPK_SgPuHdHZiIuh_RZCY6xmf4v1QR3SIMPkG6wu-hVc-r6-S7IuRM8zyuFWZD_OLzaF_w-PbeYyiesv5M5dn68Mx5-BsPmjazv8Y0ApqmLLOa4-RxYjxmmg1kT2GXWcJ-Dij-rptGb_FZcc5U7p0revDsvLReIVBGv2BuFTbMklWsM0cUeFytEBCPW8rfuaYqmG4uCg90ISOrP2Rm-YZtfGy5vRT1rlVcCtweqLx61W0X-Zf0vthYM-3ik76aW4Wcl5LyGV9M-KkBFjA0oYn1MXfgxaHaqS0MotyLAffQRd6BrGDoOjiwJVXYxzNh3_Q',
   'Content-Type': 'application/json'
@@ -21,7 +27,7 @@ def health_check():
     return 'HTN'
 
 
-@app.route('/messages', methods=['GET', 'POST'])
+@app.route('/messages', methods=['POST'])
 def receive_message():
     data = request.get_json()
     print(f'Data received from Vonage: {json.dumps(data)}')
